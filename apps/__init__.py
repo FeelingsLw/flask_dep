859527a3ebad10017c27c6640abacaa1cf1238d1
feelingslw <205339495@qq.com>
2019-11-26 17:13:00 +0800

    增加了home主页

--- apps/__init__.py
+++ apps/__init__.py
@@ -9,3 +9,17 @@ def index():
 @app.route('/home')
 def home():
     return '主页!!!'
+
+'''
+git init 初始化参数
+git add file  增加要上传文件
+-- 可选
+    --   git config --global user.email "you@example.com"  设置上传的邮箱
+    --   git config --global user.name "Your Name"  设置上传的用户名
+git commit -m '备注信息'  提交修改到本地仓库
+-- 可选
+    -- git remote add origin https://github.com/FeelingsLw/flask_dep.git  # 设置要向哪个网络仓库上传
+
+git push origin master  上传到网络仓库
+
+'''
